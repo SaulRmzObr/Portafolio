60ffe2d4ed3b14545df01ef04821f9b0233d9c86
SaulRmzObr <saul.ramirez.obregon@gmail.com>
2022-09-20 22:25:57 -0500

    Se crean entidades para pasarle un modelo a la vista Index del controller Home

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -15,7 +15,40 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var proyectos = ObtenerProyectos();
+            var modelo = new HomeIndexViewModel { Proyectos = proyectos };
+            return View(modelo);
+        }
+
+        private List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto
+                {
+                    Titulo = "Api Gmm",
+                    Descripcion = "Api Rest para cotizar seguros de Gmm",
+                    Link = "https://api.aarco.com.mx/cotizarpaquetesgmm",
+                    ImagenURL ="/img/restapigmm.png"
+
+                },
+                new Proyecto
+                {
+                    Titulo = "Landing Gmm",
+                    Descripcion = "Sitio web personalizado para agentes donde puedan cotizar seguros de gmm",
+                    Link = "https://cotizamaticos.com/lan.gmm/cotizamatico.prueba",
+                    ImagenURL ="/img/landinggmm.png"
+
+                },
+                new Proyecto
+                {
+                    Titulo = "Emision Mapfre Gmm",
+                    Descripcion = "Modulo del cotizador de gmm donde pueden emitir cotizaciones del producto Pmm de Gmm",
+                    Link = "https://uat.cotizamatico.com.mx/login",
+                    ImagenURL ="/img/emisiongmm.png"
+
+                }
+            };
         }
 
         public IActionResult Privacy()
