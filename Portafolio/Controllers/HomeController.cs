89e9a96e4dcd490be14c0fe991e6b4f8f1751e3f
SaulRmzObr <saul.ramirez.obregon@gmail.com>
2022-09-24 21:47:58 -0500

    Se agregan nuevas acciones y se agrega como parametro de entrada en el constructor el servicio de enviar correos

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -9,26 +9,47 @@ namespace Portafolio.Controllers
     public class HomeController : Controller
     {
         private readonly IRepositorioProyectos _repositorioProyectos;
+        private readonly IServicioEmail _servicioEmail;
 
         public HomeController(
-            IRepositorioProyectos repositorioProyectos
+            IRepositorioProyectos repositorioProyectos,
+            IServicioEmail servicioEmail
             )
         {
             _repositorioProyectos = repositorioProyectos;
+            _servicioEmail = servicioEmail;
         }
 
         public IActionResult Index()
         {
-            var proyectos = _repositorioProyectos.ObtenerProyectos();
+            var proyectos = _repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexViewModel 
             {
                 Proyectos = proyectos
             };
             return View(modelo);
         }
-        public IActionResult Privacy()
+        public IActionResult Contacto()
         {
             return View();
+        }
+
+        [HttpPost] //atributo
+        public async Task<IActionResult> Contacto(ContactoViewModel contactoViewModel)
+        {
+            await _servicioEmail.Enviar(contactoViewModel);
+            return RedirectToAction("Gracias");
+        }
+
+        public IActionResult Gracias()
+        {
+            return View();
+        }
+
+        public IActionResult Proyectos()
+        {
+            var proyectos = _repositorioProyectos.ObtenerProyectos();
+            return View(proyectos);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
